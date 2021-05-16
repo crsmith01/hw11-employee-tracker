e9041cd836d3b7630106d24c4c6ad2284db78074
Catherine Smith <catsmith24601@gmail.com>
2021-05-15 22:37:00 -0400

    package.json updated

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   port: 3306,
   user: 'root',
   password: 'process.env.MYSQL_PASSWORD',
-  database: 'employees'
+  database: 'employeeTrackerDB'
 });
 
 module.exports = connection;
